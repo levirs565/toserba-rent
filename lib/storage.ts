0fd795536a5ea3467ef8714745ebb012bb7b3b26
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-12-19 13:51:35 +0700

    feat: support vercel blob

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,11 +1,20 @@
 import { createStorage } from "unstorage";
 import fsDriver from "unstorage/drivers/fs";
+import vercelBlobDriver from "unstorage/drivers/vercel-blob";
 import path from "node:path";
 
-const basePath = path.resolve(process.cwd(), process.env.STORAGE_PATH ?? ".storage");
+const useVercelBlob = process.env.VERCEL_BLOB == "true";
+const basePath = path.resolve(
+  process.cwd(),
+  process.env.STORAGE_PATH ?? ".storage"
+);
 
 export const storage = createStorage({
-  driver: fsDriver({
-    base: basePath,
-  }),
+  driver: useVercelBlob
+    ? vercelBlobDriver({
+        access: "public",
+      })
+    : fsDriver({
+        base: basePath,
+      }),
 });
